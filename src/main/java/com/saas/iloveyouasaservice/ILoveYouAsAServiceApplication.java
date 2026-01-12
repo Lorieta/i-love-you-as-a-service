a44b604c1cd682b27003c91f73cc3cec6471f697
Lorieta <johncarlo.lorieta@gmail.com>
2026-01-11 21:06:37 -0800

    Init

---
+++ src/main/java/com/saas/iloveyouasaservice/ILoveYouAsAServiceApplication.java
@@ -0,0 +1,13 @@
+package com.saas.iloveyouasaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ILoveYouAsAServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ILoveYouAsAServiceApplication.class, args);
+    }
+
+}
